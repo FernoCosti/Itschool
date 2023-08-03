d3c23c054dda84129bbb1344f9f370ed495348a2
FernoCosti <ferno.costi@gmail.com>
2023-08-03 20:58:37 +0300

    Session 3 Practice

---
+++ src/session3/ScannerClass.java
@@ -0,0 +1,10 @@
+package session3;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/session3/UnaryOperators.java
@@ -0,0 +1,9 @@
+package session3;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int number = -10;
+        number = -number;
+    }
+}
