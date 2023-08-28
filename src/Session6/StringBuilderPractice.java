89134837a82ae28c198da453346cef159f49192d
FernoCosti <ferno.costi@gmail.com>
2023-08-28 15:44:40 +0300

    Session 6 partial Homework

---
+++ src/Session6/StringBuilderPractice.java
@@ -0,0 +1,19 @@
+package Session6;
+
+public class StringBuilderPractice {
+
+    public static void main(String[] args) {
+        String result = "";
+        for (int index = 0; index < 10; index++) {
+            result += "a";
+        }
+        System.out.println(result);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int index = 0; index < 10; index++) {
+            stringBuilder.append("a");
+        }
+        String result2 = stringBuilder.toString();
+        System.out.println(result2);
+    }
+}
