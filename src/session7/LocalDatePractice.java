401453238b582db07fb3cb2dd9c643b0c0326f86
FernoCosti <ferno.costi@gmail.com>
2023-09-07 06:16:28 +0300

    Session 7 Completed

---
+++ src/session7/LocalDatePractice.java
@@ -0,0 +1,22 @@
+package session7;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class LocalDatePractice {
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalDate localDateIncrement = localDate.plusDays(10);
+        System.out.println(localDateIncrement);
+
+        System.out.println(findNextSunday(LocalDate.now()));
+    }
+
+    //Find out the date of the next Sunday from a given date.
+    public static LocalDate findNextSunday(LocalDate localDate) {
+        return localDate.plusDays(DayOfWeek.SUNDAY.getValue() - localDate.getDayOfWeek().getValue());
+    }
+}
---
+++ src/session7/MathPractice.java
@@ -0,0 +1,17 @@
+package session7;
+
+public class MathPractice {
+
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 10;
+
+        int min = Math.min(firstNumber, secondNumber);
+        int max = Math.max(firstNumber, secondNumber);
+
+        System.out.println("min number: " + min);
+        System.out.println("max number: " + max);
+
+        System.out.println(Math.PI);
+    }
+}
